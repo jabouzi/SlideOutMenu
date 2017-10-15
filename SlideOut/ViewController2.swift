134ce70b797129528478012e9a229b83ec7758c1
Skander Jabouzi <jabouzi@gmail.com>
2017-10-15 09:22:03 -0400

    make slide menu works

--- SlideOut/ViewController2.swift
+++ SlideOut/ViewController2.swift
@@ -9,21 +9,49 @@
 import UIKit
 
 class ViewController2: UIViewController {
-    var menuView: MenuView!
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        menuView = MenuView(frame: CGRect(x: 0, y: 0, width: 240, height: 667))
-//        let childViewController = UIStoryboard(name: "Main2", bundle: nil).instantiateViewController(withIdentifier: "menuViewController");
-//        self.addChildViewController(childViewController)
-//        self.view.addSubview(childViewController.view)
-//        childViewController.didMove(toParentViewController: self)
-        // Do any additional setup after loading the view, typically from a nib.
+  var menuView: MenuView!
+  var leadingConstraint: NSLayoutConstraint!
+  var topConstraint: NSLayoutConstraint!
+  var bottomConstraint: NSLayoutConstraint!
+  var menuShowing = false
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    menuView = MenuView(frame: CGRect(x: 0, y: 0, width: 240, height: 667))
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    self.view.addSubview(menuView)
+    menuView.translatesAutoresizingMaskIntoConstraints = false
+    setConstraints()
+    leadingConstraint.constant = -340
+    menuView.layer.shadowOpacity = 1
+    menuView.layer.shadowRadius = 1
+  }
+  
+  private func setConstraints() {
+    leadingConstraint = NSLayoutConstraint(item: menuView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
+    topConstraint = NSLayoutConstraint(item: menuView, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0)
+    bottomConstraint = NSLayoutConstraint(item: menuView, attribute: .bottom, relatedBy: .equal, toItem: self.bottomLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
+
+    view.addConstraint(leadingConstraint)
+    view.addConstraint(topConstraint)
+    view.addConstraint(bottomConstraint)
+  }
+  
+  @IBAction func openMenu(_ sender: Any) {
+    if (menuShowing == false) {
+      leadingConstraint.constant = 0
+    } else {
+      leadingConstraint.constant = -240
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        self.view.addSubview(menuView)
-    }
+    UIView.animate(withDuration: 0.3, animations: {
+      self.view.layoutIfNeeded()
+    })
+    menuShowing = !menuShowing
+  }    
+  
 }
 
 
